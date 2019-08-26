1c378d92514dc173d57eb59c2574d87d7a7fc623
Dan-Sorin <dan_osorhean@yahoo.com>
2019-08-26 21:33:15 +0300

    studied classes, variables, primitive, data types and aggregation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,8 +6,43 @@ package org.fasttrackit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+    public static void main( String[] args ) {
+        // this is a one-line comment
+
+        Car car = new Car();
+        car.name = "Dacia";
+        car.fuelLevel = 60;
+        car.mileage = 8.5;
+        car.doorCount = 5;
+        car.maxSpeed = 180;
+        car.fuelType = "Gasoline";
+
+        Engine engine = new Engine ();
+        engine.manufacturer  = "Renault"
+        engine capacity = 1600
+
+          car,engine = engine
+
+        System.out.println(car.name);
+        System.out.println(car.travelDistance);
+        System.out.println(car.running);
+
+        Car car2 = new Car ();
+        car2.name = "Golf";
+        car.fuelLevel = 50;
+        car.mileage = 7.5;
+        car.doorCount = 3;
+        car.maxSpeed = 200;
+        car.fuelType = "Gasoline";
+
+        car.name = "Skoda";
+
+         //concatenation
+        System.out.println("Car2.name: " + car2.name);
+        System.out.println("Car2.name: " + car.name);
+
+        car = car2;
+
+
+    }}
+
